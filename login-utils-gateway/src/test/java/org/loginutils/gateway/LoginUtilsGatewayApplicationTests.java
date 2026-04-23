8516b97bc14f2171497a1576ed98aea89018cf93
Kai <sax0601@gmail.com>
2026-04-23 11:14:09 +0800

    Remove the gateway in Maven

--- login-utils-gateway/src/test/java/org/loginutils/gateway/LoginUtilsGatewayApplicationTests.java
+++ login-utils-gateway/src/test/java/org/loginutils/gateway/LoginUtilsGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package org.loginutils.gateway;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LoginUtilsGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package org.loginutils.gateway;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LoginUtilsGatewayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
--- login-utils-mgr/src/test/java/org/loginutils/mgr/UserRepositoryTest.java
+++ login-utils-mgr/src/test/java/org/loginutils/mgr/UserRepositoryTest.java
@@ -1,31 +1,31 @@
-package org.loginutils.mgr;
-
-import org.junit.jupiter.api.Test;
-import org.loginutils.dal.mappers.UserMapper;
-import org.loginutils.dal.model.UserDo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-//@SpringBootTest(classes = DalConfig.class)
-@SpringBootTest
-public class UserRepositoryTest {
-
-    @Autowired
-    private UserMapper userMapper;
-
-    @Test
-    public void testCreateUser() {
-        UserDo user = new UserDo();
-        user.setUsername("test02");
-        user.setPassword("pass123");
-        user.setStatus(1);
-        userMapper.insert(user);
-        System.out.println("OK");
-    }
-
-    @Test
-    void testFindByUsername() {
-//        UserDo user = userMapper.findList("kai");
-//        System.out.println(user.getUsername());
-    }
-}
+//package org.loginutils.mgr;
+//
+//import org.junit.jupiter.api.Test;
+//import org.loginutils.dal.mappers.UserMapper;
+//import org.loginutils.dal.model.UserDo;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+////@SpringBootTest(classes = DalConfig.class)
+//@SpringBootTest
+//public class UserRepositoryTest {
+//
+//    @Autowired
+//    private UserMapper userMapper;
+//
+//    @Test
+//    public void testCreateUser() {
+//        UserDo user = new UserDo();
+//        user.setUsername("test02");
+//        user.setPassword("pass123");
+//        user.setStatus(1);
+//        userMapper.insert(user);
+//        System.out.println("OK");
+//    }
+//
+//    @Test
+//    void testFindByUsername() {
+////        UserDo user = userMapper.findList("kai");
+////        System.out.println(user.getUsername());
+//    }
+//}
